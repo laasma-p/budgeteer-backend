c522a4b317346aef0ced9650ab5c35532cb6ca6d
Lasma Poksane <laasmapoksane@gmail.com>
2025-07-24 16:40:14 +0200

    Render budget routes

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const budgetRoutes = require("./routes/budget-routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API point is working.");
-});
+app.use("/budget", budgetRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on PORT ${process.env.PORT}...`);
